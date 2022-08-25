3a35f9657d77616f6dc3592af86952f0373e83b8
james-leonard <merrymerry1320@gmail.com>
2022-08-25 19:56:38 +0100

    added email

--- pentcrm/settings.py
+++ pentcrm/settings.py
@@ -136,8 +136,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 django_heroku.settings(locals())
 
 AUTH_USER_MODEL = 'leads.User'
-#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'chiagoziemleonard@gmail.com'
+EMAIL_HOST_PASSWORD = 'mxzkbclnektassey'
 LOGIN_REDIRECT_URL ="/leads"
 LOGIN_URL = "/login"
 LOGOUT_REDIRECT_URL = "/"
